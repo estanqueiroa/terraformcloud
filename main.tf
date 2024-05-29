2461e095f0093580af48c0604b19181664165804
estanqueiroa <artur@estanqueiro.com.br>
2024-05-29 09:57:32 -0300

    1.5.0

--- main.tf
+++ main.tf
@@ -1,40 +1,40 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+# resource "aws_vpc" "main" {
+#   cidr_block = "10.0.0.0/16"
 
-  tags = {
-    Name = "Project VPC"
-  }
-}
+#   tags = {
+#     Name = "Project VPC"
+#   }
+# }
 
-provider "aws" {
-  region = "us-east-1"
+# provider "aws" {
+#   region = "us-east-1"
 
-  default_tags {
-    tags = {
-      # mandatory tags
-      Product        = "product02"
-      APM_functional = "functional123"
-      CIA            = "ciadoassado"
+#   default_tags {
+#     tags = {
+#       # mandatory tags
+#       Product        = "product02"
+#       APM_functional = "functional123"
+#       CIA            = "ciadoassado"
 
-    }
-  }
+#     }
+#   }
 
-}
+# }
 
 
-resource "aws_s3_bucket" "test" {
-  bucket = "tf-cloud-github-idp-2024"
+# resource "aws_s3_bucket" "test" {
+#   bucket = "tf-cloud-github-idp-2024"
 
-  tags = {
-    Name        = "My bucket2"
-    Environment = "PROD4"
-    Terraform   = "Cloud"
-  }
-}
+#   tags = {
+#     Name        = "My bucket2"
+#     Environment = "PROD4"
+#     Terraform   = "Cloud"
+#   }
+# }
 
-resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.test.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# resource "aws_s3_bucket_versioning" "versioning_example" {
+#   bucket = aws_s3_bucket.test.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
